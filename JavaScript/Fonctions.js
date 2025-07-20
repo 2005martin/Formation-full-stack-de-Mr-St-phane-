05f0ed384c677270a890c11a9272b5e3ac06b8a0
2005martin <156318380+2005martin@users.noreply.github.com>
2025-07-21 00:04:48 +0100

    Add files via upload

---
+++ JavaScript/Fonctions.js
@@ -0,0 +1,14 @@
+function diremerci() {
+  console.log("je te remercie");
+}
+diremerci();
+function diremerciàquelquun(nom) {
+  console.log("je te remercie " +nom);
+}
+diremerciàquelquun("patrick");
+
+function puissance(a,b){
+  return a**b;
+}
+let resultat=puissance(2,3)
+console.log(resultat)

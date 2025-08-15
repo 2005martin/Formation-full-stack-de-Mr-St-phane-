aa2c9a244af25c96babac0b1a87f9a3369b99b80
2005martin <martinmvomoebolo45@gmail.com>
2025-08-15 19:41:39 +0200

    Mise à jour et suppressions de fichiers

--- JavaScript/Conversion_temperature.js
+++
@@ -1,8 +0,0 @@
-function convertirEnFahrenheit(celsius) {
-  return (celsius * 9 / 5) + 32;
-}
-
-let temperature = prompt("veillez une temperature en convertir pour convertir en fahrenheit");
-temperature=parseFloat(temperature);
-let resultat=convertirEnFahrenheit(temperature);
-console.log(resultat);  
--- JavaScript/Gerenateur_de_tables_de_multiplication.js
+++
@@ -1,6 +0,0 @@
-let nombre = prompt("entrez le nombre");
-let i = 1;
-nombre=parseInt(nombre);
-for(i=1; i<=10; i++){
-  console.log(`${nombre} * ${i} = ${nombre*i}`);
-}
